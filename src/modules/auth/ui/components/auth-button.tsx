36a3ed6c9392bbc2a4f912ac1ce148d04c4d4b5f
FebrianNugraha <febrian.14nugraha@gmail.com>
2025-08-14 15:57:44 +0700

    feat: auth

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,11 +1,32 @@
 import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
 import { UserCircle2 } from "lucide-react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { UserButton } from "./user-button";
+import Link from "next/link";
 
-export const AuthButton = () => {
+export const AuthButton = async () => {
+
+
+    const session = await auth.api.getSession({
+        headers: await headers(),
+      });
+    
+
     return (
-        <Button className="rounded-full text-blue-500 font-semibold" variant={"outline"}>
-            <UserCircle2/>
-            Sign In
-        </Button>
+        <>
+            
+                {session ? (
+                    <UserButton/>
+                ) : (
+                    <Button className="rounded-full text-blue-500 font-semibold" variant={"outline"}>
+                        <Link href="/sign-in" className="flex items-center gap-2">
+                            <UserCircle2 className="w-5 h-5" />
+                            Sign In
+                        </Link>
+                    </Button>
+                )}
+        </>
     );
 }  
---
+++ src/modules/auth/ui/components/user-button.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Skeleton } from "@/components/ui/skeleton";
+import { authClient } from "@/lib/auth-client";
+import { LogOut, User } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export const UserButton = () => {
+  const router = useRouter();
+  const { data } = authClient.useSession();
+
+  if (!data || !data.user) {
+    return <Skeleton className="h-15 w-full rounded-md" />;
+  }
+
+  const onLogout = () => {
+    authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/sign-in");
+        },
+      },
+    });
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center justify-center rounded-full">
+        <Avatar className="h-10 w-10 rounded-full">
+          <Image src={data.user.image!} height={50} width={50} alt="test" />
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" side="bottom" className="w-82">
+        <DropdownMenuLabel>
+          <div className="flex flex-row gap-2">
+            <Avatar className="h-9 w-9 rounded-full">
+              <Image src={data.user.image!} height={50} width={50} alt="test" />
+            </Avatar>
+            <div className="flex flex-col">
+              <span className="font-medium text-sm">{data.user.name}</span>
+              <span className="text-sm text-muted-foreground font-normal truncate">
+                {data.user.email}
+              </span>
+              <Link
+                href={`/users/${data.user.id}`}
+                className="text-blue-500 mt-2"
+              >
+                View Profile
+              </Link>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="flex items-center cursor-pointer pl-4">
+          <LogOut className="size-5" />
+          <span className="ml-2 text-[16px]" onClick={onLogout}>
+            Logout
+          </span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
