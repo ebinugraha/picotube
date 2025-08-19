f357ff0ec2b9bdcdf6988850a822e88e13b76050
FebrianNugraha <febrian.14nugraha@gmail.com>
2025-08-19 11:41:19 +0700

    feat: infinity loading

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -7,26 +7,25 @@ import { UserButton } from "./user-button";
 import Link from "next/link";
 
 export const AuthButton = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-
-    const session = await auth.api.getSession({
-        headers: await headers(),
-      });
-    
-
-    return (
-        <>
-            
-                {session ? (
-                    <UserButton/>
-                ) : (
-                    <Button className="rounded-full text-blue-500 font-semibold" variant={"outline"}>
-                        <Link href="/sign-in" className="flex items-center gap-2">
-                            <UserCircle2 className="w-5 h-5" />
-                            Sign In
-                        </Link>
-                    </Button>
-                )}
-        </>
-    );
-}  
+  return (
+    <>
+      {session ? (
+        <UserButton />
+      ) : (
+        <Button
+          className="rounded-full text-blue-500 font-semibold"
+          variant={"outline"}
+        >
+          <Link href="/sign-in" className="flex items-center gap-2">
+            <UserCircle2 className="w-5 h-5" />
+            Sign In
+          </Link>
+        </Button>
+      )}
+    </>
+  );
+};
--- src/modules/studio/ui/components/videos-section.tsx
+++ src/modules/studio/ui/components/videos-section.tsx
@@ -7,11 +7,36 @@ import {
 } from "@tanstack/react-query";
 import { videos } from "../../../../db/schema";
 import { DEFAULT_LIMIT } from "@/constant";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export const VideosSection = () => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+export const VideosSectionSuspense = () => {
   const trpc = useTRPC();
 
-  const { data } = useSuspenseInfiniteQuery(
+  const router = useRouter();
+
+  const data = useSuspenseInfiniteQuery(
     trpc.studio.getMany.infiniteQueryOptions(
       {
         limit: DEFAULT_LIMIT,
@@ -22,5 +47,45 @@ export const VideosSection = () => {
     )
   );
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="pl-6 w-[510px]">Videos</TableHead>
+            <TableHead>Visibility</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead className="text-right">Views</TableHead>
+            <TableHead className="text-right">Comments</TableHead>
+            <TableHead className="pr-6 text-right">Likes</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.data.pages
+            .flatMap((page) => page.items)
+            .map((video) => (
+              <TableRow
+                key={video.id}
+                onClick={() => router.push(`/studio/videos/${video.id}`)}
+              >
+                <TableCell className="pl-6 w-[510px]">{video.title}</TableCell>
+                <TableCell>Visibility</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Date</TableCell>
+                <TableCell className="text-right">Views</TableCell>
+                <TableCell className="text-right">Comments</TableCell>
+                <TableCell className="pr-6 text-right">Likes</TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+      <InfiniteScroll
+        isManual
+        fetchNextPage={data.fetchNextPage}
+        hasNextPage={data.hasNextPage}
+        isFetchingNextPage={data.isFetchingNextPage}
+      />
+    </div>
+  );
 };
