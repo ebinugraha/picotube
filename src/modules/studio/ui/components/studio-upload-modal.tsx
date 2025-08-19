e7b604ee4c6af912c585e89065784895b0cf3704
FebrianNugraha <febrian.14nugraha@gmail.com>
2025-08-19 14:54:27 +0700

    feat: mux integration

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { ResponsiveDialog } from "@/components/responsive-dialog";
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Loader2, PlusIcon } from "lucide-react";
+import { useState } from "react";
 import { toast } from "sonner";
+import { StudioUploader } from "./studio-uploader";
 
 export const StudioUploadModal = () => {
   const trpc = useTRPC();
+  const [isOpen, setIsOpen] = useState(false);
   const queryClient = useQueryClient();
 
   const create = useMutation(
@@ -27,13 +31,26 @@ export const StudioUploadModal = () => {
   );
 
   return (
-    <Button
-      variant={"secondary"}
-      onClick={() => create.mutate()}
-      disabled={create.isPending}
-    >
-      {create.isPending ? <Loader2 className="animate-spin" /> : <PlusIcon />}
-      Create
-    </Button>
+    <>
+      <ResponsiveDialog
+        title=" Upload a video"
+        open={!!create.data?.url}
+        onOpenChange={() => create.reset()}
+      >
+        {create.data?.url ? (
+          <StudioUploader endpoint={create.data.url} onSuccess={() => {}} />
+        ) : (
+          <Loader2 className="animate-spin" />
+        )}
+      </ResponsiveDialog>
+      <Button
+        variant={"secondary"}
+        onClick={() => create.mutate()}
+        disabled={create.isPending}
+      >
+        {create.isPending ? <Loader2 className="animate-spin" /> : <PlusIcon />}
+        Create
+      </Button>
+    </>
   );
 };
--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,5 +1,6 @@
 import db from "@/db";
 import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { and, desc, eq, lt, or } from "drizzle-orm";
 import z from "zod";
@@ -8,15 +9,26 @@ export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.session.user;
 
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policies: ["public"],
+      },
+      cors_origin: "*",
+    });
+
     const [video] = await db
       .insert(videos)
       .values({
         userId,
         title: "untilted",
+        muxStatus: "waiting",
+        muxUploadId: upload.id,
       })
       .returning();
     return {
       video: video,
+      url: upload.url,
     };
   }),
 });
